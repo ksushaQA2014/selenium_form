5b4302a71bffff9c11c9fbd3e1c25ceff59896c4
ksushaQA2014 <gryshyna.ksusha@gmail.com>
2020-07-27 21:47:13 +0300

    My second test

--- selenium_form.py
+++ selenium_form.py
@@ -1,6 +1,19 @@
-# Open https://the-internet.herokuapp.com/login
+driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
+time.sleep(5)
+if driver.find_element_by_id("at-cv-lightbox-close").size != 0:
+   driver.find_element_by_id("at-cv-lightbox-close").click()
 
-# Please automate next test cases:
-# 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
-# 2. Login with invalid creds and check validation error
-# 3. Logout from app and assert you successfully logged out
+
+driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['single_input'])
+driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
+
+assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
+
+driver.find_element(By.CSS_SELECTOR, "#sum1").send_keys(TEST_DATA['two_input_first'])
+driver.find_element(By.CSS_SELECTOR, "#sum2").send_keys(TEST_DATA['two_input_second'])
+driver.find_element(By.CSS_SELECTOR, "form#gettotal button").click()
+
+assert driver.find_element(By.CSS_SELECTOR, "#displayvalue").text == str(TEST_DATA['two_input_first'] + TEST_DATA['two_input_second'])
+
+
+driver.quit()
