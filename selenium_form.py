88a0b8256a0b28cbf9744273bcc2815636ea9e87
ksushaQA2014 <gryshyna.ksusha@gmail.com>
2020-07-27 22:53:14 +0300

    Gryshyna selenium basic + advanced

--- selenium_form.py
+++ selenium_form.py
@@ -32,3 +32,46 @@ assert driver.find_element(By.CSS_SELECTOR, "#displayvalue").text == str(TEST_DA
 driver.quit()
 
 
+Часть Advanced
+
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+TEST_DATA1 = {
+        'username': 'tomsmith',
+        'password': 'SuperSecretPassword!',
+
+    }
+
+TEST_DATA2 = {
+        'username': 'tomsmith23',
+        'password': 'SuperSecretPassword!',
+
+    }
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+
+driver.find_element(By.CSS_SELECTOR, "#username").send_keys(TEST_DATA1['username'])
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys(TEST_DATA1['password'])
+driver.find_element(By.CSS_SELECTOR, "#login > button").click()
+
+assert 'You logged into a secure area!' in driver.page_source
+driver.quit()
+
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+
+driver.find_element(By.CSS_SELECTOR, "#username").send_keys(TEST_DATA2['username'])
+driver.find_element(By.CSS_SELECTOR, "#password").send_keys(TEST_DATA2['password'])
+driver.find_element(By.CSS_SELECTOR, "#login > button").click()
+
+assert 'Your username is invalid!' in driver.page_source
+driver.quit()
+
+
+
